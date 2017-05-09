6853f4effc5b80692ce2fa58dec7a965d4efa4bc
Jon Hunter <jonhunter1977@gmail.com>
2017-05-09 08:36:21 +0100

    fixing build attempt 1

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,9 +3,13 @@
 set -o nounset
 set -o errexit
 
-echo "Building and tagging image"
+echo "Building image"
 sudo docker build --rm=false -t $DOCKER_USER/hr-react:$CIRCLE_SHA1 -t $DOCKER_USER/hr-react:latest .
+
+echo "Tagging image"
+# sudo docker tag hr-react $DOCKER_USER/hr-react:$CIRCLE_SHA1
+# sudo docker tag hr-react $DOCKER_USER/hr-react:latest
 
 echo "Pushing image to registry"
 sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-sudo docker push $DOCKER_USER/hr-react:$CIRCLE_SHA1
+sudo docker push $DOCKER_USER/hr-react:latest
