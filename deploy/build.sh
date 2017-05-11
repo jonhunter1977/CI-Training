bc0c4beeb3554f2ad583eb59767c07bd0dddf9ea
Jon Hunter <jonhunter1977@users.noreply.github.com>
2017-05-11 11:18:52 +0100

    Update build.sh

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,8 +4,8 @@ set -o nounset
 set -o errexit
 
 echo "Building image"
-sudo docker build --rm=false -t $DOCKER_USER/hr-react:$CIRCLE_SHA1 -t $DOCKER_USER/hr-react:latest .
+sudo docker build --rm=false -t $DOCKER_USER/CI-training:$CIRCLE_SHA1 -t $DOCKER_USER/CI-training:latest .
 
 echo "Pushing image to registry"
 sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-sudo docker push $DOCKER_USER/hr-react:latest
+sudo docker push $DOCKER_USER/CI-training:latest
