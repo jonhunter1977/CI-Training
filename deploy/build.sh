1f0a1c5d3915b015b3074a26caa25e28d368246b
Jon Hunter <jonhunter1977@gmail.com>
2017-05-11 12:33:22 +0100

    change docker repo name to lowercase

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,8 +4,8 @@ set -o nounset
 set -o errexit
 
 echo "Building image"
-sudo docker build --rm=false -t $DOCKER_USER/CI-training:$CIRCLE_SHA1 -t $DOCKER_USER/CI-training:latest .
+sudo docker build --rm=false -t $DOCKER_USER/ci-training:$CIRCLE_SHA1 -t $DOCKER_USER/ci-training:latest .
 
 echo "Pushing image to registry"
 sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-sudo docker push $DOCKER_USER/CI-training:latest
+sudo docker push $DOCKER_USER/ci-training:latest
