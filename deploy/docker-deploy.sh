25b1d4a9517743e9329c7b436a4bfda926d3a5f9
Jon Hunter <jonhunter1977@gmail.com>
2017-05-11 14:09:54 +0100

    added a nice marquee

--- deploy/docker-deploy.sh
+++ deploy/docker-deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo docker pull jonhunter1977/ci-training:latest
-sudo docker kill ci-training
+sudo docker rename ci-training ci-training-old
+sudo docker kill ci-training-old
+sleep 2s
 sudo docker run -d -p 4001:4001 --name ci-training jonhunter1977/ci-training:latest
+sudo docker rm ci-training-old
