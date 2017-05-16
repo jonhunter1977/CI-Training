44449104bb3977716de6d1db0d8e6d8b6a0bba87
gabberdemon <pamelanaylor88@googlemail.com>
2017-05-16 15:06:14 +0100

    check test

---
+++ test/express/helpers/subtract-2-numbers-test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const subtractTwoNumbers = require('../../../express/helpers/subtract-2-numbers.js');
+const should = require('should');
+
+describe('subtractTwoNumbers', () => {
+
+  it('should subtract one number from another number correctly', () => {
+    const result = subtractTwoNumbers(10, 3);
+    should.equal(result, 7);
+  })
+});
