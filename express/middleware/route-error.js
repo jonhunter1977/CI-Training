99e09b8fdd929deb17641497b351cc8acdad7256
Jon Hunter <jonhunter1977@gmail.com>
2017-05-16 09:58:38 +0100

    add test to fix coverage

--- express/middleware/route-error.js
+++ express/middleware/route-error.js
@@ -1,6 +1,5 @@
 'use strict';
 
 module.exports = (err, req, res, next) => { // jshint ignore:line
-  res.status(err.code || 500);
-  res.send(err.message || 'Oops something went wrong');
+  res.status(err.code || 500).send(err.message || 'Oops something went wrong');
 };
